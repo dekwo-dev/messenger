52a2275d772d957fcd0817913278b098a5457b87
Dekr0 <dekr0.dk@protonmail.com>
2024-03-19 13:35:22 -0600

    unused const

--- const.go
+++ const.go
@@ -5,14 +5,7 @@ import (
 	"time"
 )
 
-const remote string = "172.105.103.168" // Testing / Debug
-const ipify string = "https://api.ipify.org?format=text"
-const port string = "8000"
-
-const NewComment = "on-new-comment"
-const DelComment = "on-del-comment"
-const NewSub = "on-new-sub"
-const DelSub = "on-del-sub"
+const port int = 8000
 
 const writeWait = 10 * time.Second
 
